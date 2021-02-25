c09b910d02f2e077659d27a5b321e9b7708deda3
Joe <joe@joebailey.xyz>
2021-02-25 22:52:37 +0000

    Set up base vue

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <button :click="add_color">Add another color!</button>
+    <div class="color-container">
+      <color v-for="color in colors" :key="color" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Color from "./components/color";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      colors: [1],
+    };
+  },
+  components: {
+    Color,
+  },
+  methods: {
+    add_color() {
+      this.colors.push(1);
+      console.log(this.colors);
+    },
+  },
+};
+</script>
---
+++ src/components/color.vue
@@ -0,0 +1,28 @@
+<style scoped>
+.color {
+  padding: 20px;
+  background-color: var(--color);
+}
+</style>
+
+<template>
+  <div v-if="color" class="color" :style="'--color:' + color.hex">
+    <chrome-picker v-model="color" />
+  </div>
+</template>
+
+<script>
+import { Chrome } from "vue-color";
+
+export default {
+  components: {
+    "chrome-picker": Chrome,
+  },
+  name: "color",
+  data: function () {
+    return {
+      color: "#000000",
+    };
+  },
+};
+</script>
